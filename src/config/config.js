1caacb566ccb2e505b1886fd449b4e837a68956f
krystek77 <krystian.wrona77@gmail.com>
2023-06-30 11:45:53 +0200

    Change domain

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,6 @@
 const dev = process.env.NODE_ENV === 'development' ? true : false;
 
-// export const server = dev
-//   ? 'http://localhost:4000'
-//   : 'https://pralma-api.onrender.com';
-
-  export const server = dev
+export const server = dev
   ? 'http://localhost:4000'
-  : 'https://wyposazeniepralniprzemyslowej.pl';
+  : 'https://pralma-api.onrender.com';
+
